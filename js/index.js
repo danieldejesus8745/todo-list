7d5278956abbf3c986fd2a725489b1283b146c50
daniel.ribeiro <daniel.ribeiro@cf.coop.br>
2022-01-20 22:19:01 -0300

    Desenvolvida função para salvar tasks e exibir-las em tela

---
+++ js/index.js
@@ -0,0 +1,44 @@
+const form = document.querySelector('#form');
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+let id = localStorage.getItem('id') || 1;
+const ulTasks = document.querySelector('#ul-tasks');
+
+const sendTaskObject = taskObject => {
+  taskObject.id = id;
+  id++;
+  tasks.push(taskObject);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  localStorage.setItem('id', id);
+
+  loadTasks();
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const taskObject = Object.create(null);
+  taskObject.task = e.target.task.value;
+  taskObject.status = 'pending';
+
+  sendTaskObject(taskObject);
+});
+
+const loadTasks = () => {
+  ulTasks.textContent = '';
+
+  tasks.map(task => {
+    const li = document.createElement('li');
+    const spanTask = document.createElement('span');
+    const spanIcon = document.createElement('span');
+
+    spanTask.textContent = task.task;
+    spanIcon.textContent = 'radio_button_unchecked';
+
+    spanIcon.classList.add('material-icons');
+
+    li.append(spanTask, spanIcon);
+    ulTasks.append(li);
+  });
+}
+
+loadTasks();
